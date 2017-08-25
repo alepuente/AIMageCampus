db9bb464fe14022ce3657aa939fd1bfd53032f37
Alepuente <ale3puente@gmail.com>
2017-08-24 22:29:04 -0300

    Comienzo del minero

---
+++ IAfirstclas/Assets/StateMachine/FSMachine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FSMachine : MonoBehaviour {
+
+    public static FSMachine _instance;
+    public Miner[] _miners;
+    public Deployed _deployed;
+    public int[,] _fsm;
+    public int _actualState;
+
+    [System.Serializable]
+    public class Deployed : UnityEvent<int>
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+        _deployed = new Deployed();       
+        _deployed.AddListener(SetEvent);
+    }
+    
+
+    void SetEvent(int ev)
+    {
+        if (_fsm[_actualState, ev] != -1)
+        {
+            _actualState = _fsm[_actualState, ev];
+        }
+    }
+	
+
+	void Update () {
+		
+	}
+}
---
+++ IAfirstclas/Assets/StateMachine/Miner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miner : MonoBehaviour {
+    public int _loadAmount;
+    public int _maxLoad;
+    public PathFinder _pathFinder;
+	// Use this for initialization
+	void Start () {
+        _pathFinder = GetComponent<PathFinder>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (_actualState)
+        {
+            case 1: GoToMine();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void GoToMine()
+    {
+        _pathFinder._isSearching = true;
+    }
+
+    
+}
